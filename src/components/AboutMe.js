12e98b7ca336b8811d7da65ce4dd0b1c47b7b6a4
Ehsan Khosravi <ekhosr@gmail.com>
2021-11-13 13:51:22 -0500

    About Me finished

---
+++ src/components/AboutMe.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import profile from '../images/profilePhoto.jpg'
+const styles ={
+    AboutMeStyle: {
+      paddingTop:'20px',
+      color:'#F3F4ED',
+      lineHeight: '2'
+      
+    },
+    profilePhotoStyle:{
+        width:'33.5vw'
+    }
+  }
+  
+function AboutMe(){
+    return (
+        <div>
+            <img style={styles.profilePhotoStyle} alt="profile" src={profile}></img>
+            <div style={styles.AboutMeStyle}>
+            Hello! I'm Ehsan, a Full-Stack Web Developer with a background in IT and accounting. I enjoy solving problems by creating web applications. My primarily skills are HTML, CSS, and JavaScript and I'm also comfortable using back-end tech such as Node.js. I love to work as part of a dynamic team and learn from the programming challenges!  
+            </div>
+        </div>
+        
+    );
+}
+export default AboutMe
