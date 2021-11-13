a0d96f2907105d9d30f0a71601954353e4fd78d1
Ehsan Khosravi <ekhosr@gmail.com>
2021-11-13 13:42:07 -0500

    resume file added

---
+++ src/components/Resume.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import reactDom from 'react-dom';
+
+import resume from '../images/resume.pdf';
+
+
+const styles ={
+    buttonStyle: {
+      fontSize:'18px',
+      color: '#F3F4ED'
+      
+    },
+    
+}
+function Resume(){
+    return (
+        <div>
+            <button  style={styles.buttonStyle} > <a href={resume} target='_blank' rel="noreferrer">Download My Resume</a></button>
+
+            <figure>
+                <figcaption>Front-End Skills</figcaption>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Bootstrap</li>
+                    <li>React.js</li>
+                    <li>jQuery</li>
+
+                </ul>
+            </figure>
+            <figure>
+                <figcaption>Back-End Skills</figcaption>
+                <ul>
+                    <li>Node.js</li>
+                    <li>MySQL</li>
+                    <li>MongoDB</li>
+                    <li>Mongoose</li>
+                    <li>GraphQL</li>
+
+                </ul>
+            </figure>
+        </div>
+        
+    );
+}
+export default Resume
