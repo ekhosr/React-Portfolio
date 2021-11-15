a12aed3f83b1986d3092f803976f289adcfa42e6
Ehsan Khosravi <ekhosr@gmail.com>
2021-11-14 21:28:55 -0500

    Updates

---
+++ precache-manifest.defcf1e817c835beef747d7290889c85.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f77ed228d5f337fb09b35d00d44b2b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "36b1a584b31dfdc804e2",
+    "url": "/react-portfolio/static/css/main.af15bc44.chunk.css"
+  },
+  {
+    "revision": "8cf30b83cd3eef1ebbad",
+    "url": "/react-portfolio/static/js/2.4381c68c.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-portfolio/static/js/2.4381c68c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13b7e29dbd206c21e3d",
+    "url": "/react-portfolio/static/js/3.9ee2a46f.chunk.js"
+  },
+  {
+    "revision": "36b1a584b31dfdc804e2",
+    "url": "/react-portfolio/static/js/main.fd28a18b.chunk.js"
+  },
+  {
+    "revision": "1a34db56874eb8cae11e",
+    "url": "/react-portfolio/static/js/runtime-main.67ee379b.js"
+  },
+  {
+    "revision": "74cdf9dddc6c7e5a737a0895bf766e5e",
+    "url": "/react-portfolio/static/media/bookClubCollective.74cdf9dd.png"
+  },
+  {
+    "revision": "d8d1c879f5377ee4c849bbe85e101919",
+    "url": "/react-portfolio/static/media/budgetTracker.d8d1c879.png"
+  },
+  {
+    "revision": "276d8a59fa818bdb6ef64ae78a684166",
+    "url": "/react-portfolio/static/media/marvel.276d8a59.png"
+  },
+  {
+    "revision": "e65313b60c6373de0956eb73b4c7ef86",
+    "url": "/react-portfolio/static/media/noteTaker.e65313b6.png"
+  },
+  {
+    "revision": "c796c2806b94e19b99ec13c59616f6ac",
+    "url": "/react-portfolio/static/media/profilePhoto.c796c280.jpg"
+  },
+  {
+    "revision": "b8b5f3d7e36561716ef5f23204b7cc21",
+    "url": "/react-portfolio/static/media/resume.b8b5f3d7.pdf"
+  },
+  {
+    "revision": "7e81d43ccdecc5df0e0b6426c2e2ab83",
+    "url": "/react-portfolio/static/media/techBlog.7e81d43c.png"
+  },
+  {
+    "revision": "763a5b536d357ecb29416947d76a4ee5",
+    "url": "/react-portfolio/static/media/workoutTracker.763a5b53.png"
+  }
+]);
